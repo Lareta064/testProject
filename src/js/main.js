1f14adbca5d2281f379e8bd78192492c41c355cc
Lareta <lareta064@gmail.com>
2021-03-10 19:57:03 +0300

    benefits

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,40 @@ if(mixBlock){
 			});
 		} 
 	}
-//================================END CUSTOM MIXIT UP =============
+//======================= END CUSTOM MIXIT UP =============
+//======================= FOOTER ACCORDION =============
+const accordeonTitle = document.querySelectorAll('.accordeon-item-header');
+const accordeonContent = document.querySelectorAll('.accordeon-item-content');
+let loock;
+
+for (i = 0; i < accordeonTitle.length; i++) {
+    accordeonTitle[i].addEventListener('click', function() {
+        if (!(this.classList.contains('show'))) {
+
+            for(j = 0; j < accordeonTitle.length; j++) {
+                accordeonTitle[j].classList.remove('show');
+
+                for (k = 0; k < accordeonContent.length; k++) {
+                    this.nextElementSibling;
+                    accordeonContent[k].style.maxHeight = '0';
+                }
+                
+            }
+            
+            this.classList.add('show');
+            loock = this.nextElementSibling;
+            loock.style.maxHeight = loock.scrollHeight + 'px';
+            
+        } else if (this.classList.contains('show')) {
+            
+            for(l = 0; l < accordeonTitle.length; l++) {
+                accordeonTitle[l].classList.remove('show');
+                loock.style.maxHeight = '0';
+
+            }
+        }
+    })
+}
+
 
 
