a6d8950051fc5040af9b3b7ba510606472394b2f
Viktoriya <v.matveyeva@yandex.ru>
2021-03-10 23:10:32 +0400

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,26 @@ $("#search-btn").on("click", function () {
 	$("#header-search").fadeIn();
 });
 
+$("#search-btn__mob").on("click", function () {
+	$("#header-search__mob").fadeIn();
+});
+
+// Открытие моб меню по клику на гамбургер
+const menuToggle = document.querySelector('.menu-toggle');
+const mobMenu = document.querySelector('#mobile-nav');
+if (menuToggle) {
+	menuToggle.addEventListener('click', function () {
+		if (this.classList.contains('active')) {
+			this.classList.remove('active');
+			mobMenu.classList.remove('active');
+
+		} else {
+			this.classList.add('active');
+			mobMenu.classList.add('active');
+
+		}
+	});
+}
 
 
 
