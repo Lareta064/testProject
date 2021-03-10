7d02ce9532088fbacf481043557938c1862bda21
Lareta <lareta064@gmail.com>
2021-03-10 16:02:00 +0300

    footer-column

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,7 @@
-// =============выбор дополнительных продуктов в модальном окне
-const productCard = document.querySelectorAll('.productCard-item');
-const productModal = document.querySelectorAll('.productModal');
-for(let i = 0; i < productCard.length; i++){
-	
-	productCard[i].addEventListener('click', function(e){
-		
-		let cardBody = productCard[i].querySelector('.productCard');
-		let modalImg;
-		for(item of productModal){
-			modalImg = item.querySelector('.modal-body-img img');
-			if(e.target == cardBody){
-			let cardImg =  cardBody.querySelector('img');
-			let cardImgSrc =cardImg.getAttribute('src');
-			modalImg.setAttribute('src',cardImgSrc);
-		}
-		}
-		
-		
-
-	})
-}
-/**************************** */
-function massClassTogle(elemClass, toggleClass = 'active'){
-	let elements = document.querySelectorAll(elemClass);
-	for(let i = 0; i < elements.length; i++){
-		elements[i].addEventListener('click', 
-		(e)=>{
-			e.stopPropagation();
-			e.target.classList.toggle(toggleClass);
-		})
-	}
-}
-massClassTogle('.addProduct-item');
-// ============= end выбор дополнительных продуктов в модальном окне
-
 //================================CUSTOM MIXIT UP =============
 const mixBlock = document.querySelector('#mixit-block');
 if(mixBlock){
-	const mixitBtns = mixBlock.querySelectorAll('.mixitBlock-menu button');
+	const mixitBtns = mixBlock.querySelectorAll('.mixitBlock-toggle button');
 	const mixitContents = mixBlock.querySelectorAll('.mixitBlock-content');
 		for (let i = 0; i <  mixitBtns.length; i++){
 			mixitBtns[i].addEventListener('click',
